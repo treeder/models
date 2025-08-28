6fee341e24e6756c23c8dad725dc0b53dda441d9
Travis Reeder <treeder@gmail.com>
2025-08-28 00:11:06 +0000

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # models
-A secondary JSON parser to turn fields into concrete objects. 
+
+A secondary JSON parser to turn fields into concrete objects.
+
+After a `JSON.parse(s)`, run this with `parseModel(ob)` to turn fields into objects, like so:
+
+```js
+let ob = JSON.parse(s)
+ob = parseModel(ob, User)
+```
+
+## Getting Started
+
+```sh
+npm install treeder/models
+```
+
+Or in your browser:
+
+```
+import {parseModel} from 'https://cdn.jsdelivr.net/gh/treeder/models@0/index.js'
+
+
+First define your models. You may find this familiar if you use [Lit](https://lit.dev). And these models can be used for [migrations](https://github.com/treeder/migrations), for SQLite and Cloudflare D1 via [flaregun](https://github.com/treeder/flaregun), and for the handy [api](https://github.com/treeder/api) library. 
+
+
+```js
+export class User {
+  static properties = {
+    id: {
+      type: String,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: Date,
+    },
+    updatedAt: {
+      type: Date,
+    },
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    data: {
+      type: Object,
+      // you can go deeper into Object properties, only required if you don't want JSON defaults.
+      birthday: {
+        type: Date,
+      }
+    },
+  }
+}
+```
+
+Use standard JavaScript classes:
+
+- String
+- Number
+- Boolean
+- Date
+- BigInt
+- Object
+
+To use a custom parser, use `parse` instead of `type`:
+
+```js
+{
+  balance: {
+    parse: (n) => (n ? new Big(n) : new Big(0)),
+  },
+}
+```
+
+Then call `parseModel()` with your object and your new class. 
+
+```js
+let ob = JSON.parse(s)
+ob = parseModel(ob, User)
+```
+
+That's it!
---
+++ index.js
@@ -0,0 +1,73 @@
+export function parseModel(obj, clz) {
+  if (Array.isArray(obj)) {
+    for (let ob of obj) {
+      parseProperties(ob, clz)
+    }
+  } else {
+    parseProperties(obj, clz)
+  }
+  return obj
+}
+
+function parseProperties(ob, clz) {
+  if (!ob) return
+  if (!clz) return
+  if (clz.properties) {
+    for (const propName in clz.properties) {
+      let val = ob[propName]
+      // console.log("prop:", propName, val)
+      if (!val) continue
+      let prop = clz.properties[propName]
+      ob[propName] = parseProp(val, prop)
+    }
+  } else {
+    // the object might be wrapped, so go deeper
+    for (const propName in clz) {
+      let val = ob[propName]
+      // console.log("wrap prop:", propName, val)
+      if (!val) continue
+      let prop = clz[propName]
+      parseProperties(val, prop)
+    }
+  }
+}
+
+function parseProp(val, p, sub = false) {
+  if (!val || !p) return val
+  if (p.parse) {
+    // custom parse function
+    return p.parse(val)
+  }
+  switch (p.type) {
+    case Number:
+      return new Number(val)
+    case Boolean:
+      return val == 1
+    case Date:
+      return new Date(val)
+    case BigInt:
+      return BigInt(val)
+  }
+  if (!sub) {
+    // then parse JSON objects
+    switch (p.type) {
+      case Object:
+        let v = JSON.parse(val)
+        // check if there are any sub fields we need to parse
+        for (const subProp in p) {
+          // console.log("subProp:", subProp)
+          // console.log(v)
+          v[subProp] = this.parseProp(v[subProp], p[subProp], true)
+          // console.log('after:', v)
+        }
+        return v
+      case JSON:
+        return JSON.parse(val)
+      case Array:
+        return JSON.parse(val)
+      default:
+        return val
+    }
+  }
+
+}
